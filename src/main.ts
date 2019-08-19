65ded17dc763269a32f1120ebfae1a4ff2b2d427
DragonH <a0261038@g2.usc.edu.tw>
2019-08-19 23:53:42 +0800

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import Vue from 'vue';
+import vuetify from './plugins/vuetify.js';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './routers/index.router';
+import store from './stores/index.store';
+import vuetify from './plugins/vuetify';
+
 
 Vue.config.productionTip = false;
+
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
