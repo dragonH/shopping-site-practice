22e1fef472f120cfe7d4606700a82f41a8fab8ba
DragonH <a0261038@g2.usc.edu.tw>
2019-08-22 23:55:58 +0800

    Edit app-bar

--- src/components/Home/AppBar.vue
+++ src/components/Home/AppBar.vue
@@ -1,8 +1,24 @@
 <template>
   <div class = "AppBar">
-    <v-app-bar color = "red" dark app>
-      <v-toolbar-title class = "white--text">Shop</v-toolbar-title>
-    </v-app-bar>
+    <v-card>
+      <v-app-bar color = "red" dark app extended>
+        <v-toolbar-title class = "white--text">Shop</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class = "hidden-sm-and-down">
+          <v-btn text>登入</v-btn>
+        </v-toolbar-items>
+        <v-toolbar-items class = "hidden-md-and-up">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </v-toolbar-items>
+        <template v-slot:extension>
+          <v-layout>
+            <v-flex md4 offset-md4>
+              <v-text-field label = "Search"></v-text-field>
+            </v-flex>
+          </v-layout>
+        </template>
+      </v-app-bar>
+    </v-card>
   </div>
 </template>
 
